9ce9165717b24a07540288bbf3669945e7a1560c
Boris Martin <boris.martin.be@gmail.com>
2022-08-03 18:10:53 +0200

    update 3D data

--- mapping-tester-3d/generate_meshes.sh
+++ mapping-tester-3d/generate_meshes.sh
@@ -4,7 +4,7 @@ set -e
 GENERATOR_PATH=~/Bureau/aste/tools/mesh-generators
 
 # Generate all coarse meshes,
-for RES in 0.5 0.25 0.2 #0.1 0.05 0.04 0.03 0.02
+for RES in 0.5 0.45  0.40  0.35 0.30 0.25 0.225 0.2 0.19 0.18 0.17 0.16 0.15 0.14 #0.1 0.05 0.04 0.03 0.02
 do
     echo ""
     python ${GENERATOR_PATH}/generate_unit_cube.py --mesh coarse_${RES}.vtk --resolution $RES
